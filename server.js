1593894eedf38f31ee8f7d600fb10566b618b3c7
SDann <smdann17@gmail.com>
2022-10-29 11:10:29 -0500

    Receiving an error (Error: secret option required for sessions) when trying to view deployed website to Heroku. Changed secret in the server.js as a string instead of pulling from the .env file.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
-  secret: process.env.SESS_SECRET,
+  secret: 'secretSession',
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
   },
